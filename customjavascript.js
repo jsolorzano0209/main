918525d1811cd9d57c3fa4c4fd4af2bd1b3f4fb3
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 12:06:22 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,12 +1,12 @@
 var radios = document.getElementsByName('element_0');
 
 for (var i = 0, length = radios.length; i < length; i++)
-{
+{ 
  if (radios[i].checked && radios[i].value==='$50')
  {
        document.querySelector('#f4276037_total_price').innerHTML('$62.50');
        document.querySelector('#f4276037_total_items tr th').innerHTML('$62.50');
-       document.querySelector("#f4276037_total_items tr td").innerHTML('$62.50');
+       document.querySelector('#f4276037_total_items tr td').innerHTML('$62.50');
 
   // only one radio can be logically checked, don't check the rest
   break;
