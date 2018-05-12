3ca461d5be103e63f5fe5294351c443d5db1d921
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 09:04:26 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,24 +1,25 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript">
-jQuery('input:radio[name="element_0"]').change(function() {
-     if (jQuery(this).is(':checked') && jQuery(this).val() == '$50') { 
+<script>
+$(document).ready(function () {
+        $("input:radio[name='element_0']:checked").change(function () {
+            if ($("input[name='element_0']:checked").val() == '$50') {
            
    
        
-       jQuery('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
-   jQuery('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
-         jQuery("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *50)+50; return result;});
+       $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
+   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
+         $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *50)+50; return result;});
       
      }
-if (jQuery(this).is(':checked') && jQuery(this).val() == '$100') {
-             jQuery("#f4276037_total_price").html(function(){var result= "$"+(0.25 * 100)+100; return result;});
-   jQuery('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 100)+100; return result;});
-         jQuery("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *100)+100; return result;});
+if ($("input[name='element_0']:checked").val() == '$100') {
+             $("#f4276037_total_price").html(function(){var result= "$"+(0.25 * 100)+100; return result;});
+   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 100)+100; return result;});
+         $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *100)+100; return result;});
      }
-if ($(this).is(':checked') && $(this).val() == '$500') {
-             jQuery('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
-   jQuery('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
-         jQuery('#f4276037_total_items tr td').html(function(){var result= "$"+(0.25 *500)+500; return result;});
+if ($("input[name='element_0']:checked").val() == '$500') {
+             $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
+   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
+         $('#f4276037_total_items tr td').html(function(){var result= "$"+(0.25 *500)+500; return result;});
      }
 });
+    });
 </script>
