af6d8027317d8930898abfa3e31de4390909c177
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 12:05:26 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,17 +1,16 @@
-window.onload=function() {
-var radios = document.getElementsByTagName('input');
-var value; 
-for (var i = 0; i < radios.length; i++) {
-    if (radios[i].type === 'radio' && radios[i].checked) {
-        // get value, set checked flag or do whatever you need to
-        value = radios[i].value; 
-            if(value === '$50'){
-               document.getElementById('f4276037_total_price').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
-               document.getElementById('f4276037_total_items tr th').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
-               document.getElementById("f4276037_total_items tr td").innerHTML(function(){var result= "$" +(0.25 *50)+50; return result;});
+var radios = document.getElementsByName('element_0');
 
-            }          
-       }
-    }
+for (var i = 0, length = radios.length; i < length; i++)
+{
+ if (radios[i].checked && radios[i].value==='$50')
+ {
+       document.querySelector('#f4276037_total_price').innerHTML('$62.50');
+       document.querySelector('#f4276037_total_items tr th').innerHTML('$62.50');
+       document.querySelector("#f4276037_total_items tr td").innerHTML('$62.50');
+
+  // only one radio can be logically checked, don't check the rest
+  break;
+ }
+    
 }
     
