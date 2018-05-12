5534c52853a8328915eefdac006d6060f2736304
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 11:04:34 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,5 +1,5 @@
 var radios = document.getElementsByTagName('input');
-var value;
+var value; 
 for (var i = 0; i < radios.length; i++) {
     if (radios[i].type === 'radio' && radios[i].checked) {
         // get value, set checked flag or do whatever you need to
