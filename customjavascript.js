658192e52214c4a0168c4064a9179d62f102e7b4
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-11 21:17:50 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,7 +1,7 @@
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><script type="text/javascript">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
 jQuery('input:radio[name="element_0"]').change(function() {
-     if ($(this).is(':checked') && $(this).val() == '$50') {
+     if (jQuery(this).is(':checked') && jQuery(this).val() == '$50') {
            
    
        
@@ -10,7 +10,7 @@ jQuery('input:radio[name="element_0"]').change(function() {
          jQuery("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *50)+50; return result;});
       
      }
-if ($(this).is(':checked') && $(this).val() == '$100') {
+if (jQuery(this).is(':checked') && jQuery(this).val() == '$100') {
              jQuery("#f4276037_total_price").html(function(){var result= "$"+(0.25 * 100)+100; return result;});
    jQuery('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 100)+100; return result;});
          jQuery("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *100)+100; return result;});
@@ -20,6 +20,5 @@ if ($(this).is(':checked') && $(this).val() == '$500') {
    jQuery('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
          jQuery('#f4276037_total_items tr td').html(function(){var result= "$"+(0.25 *500)+500; return result;});
      }
-
 });
 </script>
