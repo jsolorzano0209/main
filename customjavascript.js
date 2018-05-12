9beb746cd9d65045f29c606156a586e174430cfa
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 11:03:36 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,15 +1,14 @@
-if(document.getElementById('element_0_0').checked) {
-        document.getElementById('f4276037_total_price').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
-        document.getElementById('f4276037_total_items tr th').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
-        document.getElementById("f4276037_total_items tr td").innerHTML(function(){var result= "$" +(0.25 *50)+50; return result;});
-}
-if(document.getElementById('element_0_1').checked) {
-        document.getElementById("f4276037_total_price").innerHTML(function(){var result= "$" +(0.25 * 100)+100; return result;});
-        document.getElementById('f4276037_total_items tr th').innerHTML(function(){var result= "$" +(0.25 * 100)+100; return result;});
-        document.getElementById("f4276037_total_items tr td").innerHTML(function(){var result= "$" +(0.25 *100)+100; return result;});
-}
-if(document.getElementById('element_0_2').checked) {
-        document.getElementById('f4276037_total_price').innerHTML(function(){var result= "$" +(0.25 * 500)+500; return result;});
-        document.getElementById('f4276037_total_items tr th').innerHTML(function(){var result= "$" +(0.25 * 500)+500; return result;});
-        document.getElementById('f4276037_total_items tr td').innerHTML(function(){var result= "$" +(0.25 *500)+500; return result;});
+var radios = document.getElementsByTagName('input');
+var value;
+for (var i = 0; i < radios.length; i++) {
+    if (radios[i].type === 'radio' && radios[i].checked) {
+        // get value, set checked flag or do whatever you need to
+        value = radios[i].value; 
+            if(value === '$50'){
+               document.getElementById('f4276037_total_price').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
+               document.getElementById('f4276037_total_items tr th').innerHTML(function(){var result= "$" +(0.25 * 50)+50; return result;});
+               document.getElementById("f4276037_total_items tr td").innerHTML(function(){var result= "$" +(0.25 *50)+50; return result;});
+
+            }          
+    }
 }
