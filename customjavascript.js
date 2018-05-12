cb67ce6084ea5059a6a3713f97d15ff19ec78ec2
jsolorzano0209 <jsolorzano0209@gmail.com>
2018-05-12 09:15:24 -0500

    Update customjavascript.js

--- customjavascript.js
+++ customjavascript.js
@@ -1,25 +1,19 @@
-<script>
 $(document).ready(function () {
-        $("input:radio[name='element_0']:checked").change(function () {
-            if ($("input[name='element_0']:checked").val() == '$50') {
-           
-   
-       
-       $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
-   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
-         $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *50)+50; return result;});
-      
-     }
-if ($("input[name='element_0']:checked").val() == '$100') {
-             $("#f4276037_total_price").html(function(){var result= "$"+(0.25 * 100)+100; return result;});
-   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 100)+100; return result;});
-         $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *100)+100; return result;});
-     }
-if ($("input[name='element_0']:checked").val() == '$500') {
-             $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
-   $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
-         $('#f4276037_total_items tr td').html(function(){var result= "$"+(0.25 *500)+500; return result;});
+     $("input:radio[name='element_0']:checked").change(function () {
+      if ($("input[name='element_0']:checked").val() == '$50') {
+        $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
+        $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 50)+50; return result;});
+        $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *50)+50; return result;});
+      }
+      if ($("input[name='element_0']:checked").val() == '$100') {
+        $("#f4276037_total_price").html(function(){var result= "$"+(0.25 * 100)+100; return result;});
+        $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 100)+100; return result;});
+        $("#f4276037_total_items tr td").html(function(){var result= "$"+(0.25 *100)+100; return result;});
+      }
+      if ($("input[name='element_0']:checked").val() == '$500') {
+        $('#f4276037_total_price').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
+        $('#f4276037_total_items tr th').html(function(){var result= "$"+(0.25 * 500)+500; return result;});
+        $('#f4276037_total_items tr td').html(function(){var result= "$"+(0.25 *500)+500; return result;});
      }
 });
     });
-</script>
